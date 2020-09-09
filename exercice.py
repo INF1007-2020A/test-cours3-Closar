7dfbdc3daf62afe95950a9f00e8c2be2a33c6628
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:43:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,8 @@ def majuscule(mot):
         if l > 96 and l <123:
             l = l - 32
             mot = mot[0:j] + chr(l) + mot[j+1:]
-        elif
+        elif l == 130:
+            
     return mot
 
 
