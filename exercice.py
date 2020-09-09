aff5340b07999f8def635e05d981d64fcc4721b7
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:13:57 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 def capitaliser_pays(nom):
     for j in range(len(nom)):
-        if j != 0 :
-            l = ord(nom[j])
-            if l > 64 and l <= 90:
-                l = l + 32
+        l = ord(nom[j])
+        if l > 64 and l <= 90:
+            l = l + 32
                 nom = nom[0:j] + chr(l) + nom[j+1:]
 
     return nom
