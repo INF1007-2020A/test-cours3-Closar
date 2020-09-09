8f74263cdbfb1fba03e66fd75870903b77676969
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:34:29 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def capitaliser_pays(nom):
-    pl = ord(nom[0])
-    if pl >= 97 and pl <= 122:
-        pl = pl - 32
-    nom = chr(pl) + nom[1:]
-    for j in range(len(nom)):
-        if j != 0 :
-            l = ord(nom[j])
-            if l > 64 and l <= 90:
-                l = l + 32
-                nom = nom[0:j] + chr(l) + nom[j+1:]
-
-
-    return nom
+def majuscule(mot):
+    # TODO completer la fonction
+    return mot
 
 
 if __name__ == '__main__':
-    pays = [
-        'AfghanIstan',
-        'albania',
-        'algeria',
-        'AndorRa',
-        'angolA',
-        'antigua ANd barbuda',
-        'argEntina',
-        'Armenia',
-        'austrAlia',
-        'ausTria',
-        'azerBaijaN'
+    mots = [
+        'riz',
+        'cours',
+        'voiture',
+        'oiseau',
+        'bonjour',
+        'églantier',
+        'arbre'
     ]
-    for i in range(len(pays)):
-        pays[i] = capitaliser_pays(pays[i])
+    for i in range(len(mots)):
+        mots[i] = majuscule(mots[i])
 
-    print(pays)
+    print(mots)
