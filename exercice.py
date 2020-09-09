07df624241a646278a3237b44706e62e7dc9c4c0
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:18:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def capitaliser_pays(nom):
         if j != 0 :
             l = ord(nom[j])
             if l >= 97 and l <= 122:
-                l = l 32 32
+                l = l - 32
                 nom = nom[0:j] + chr(l) + nom[j+1:]
 
 
