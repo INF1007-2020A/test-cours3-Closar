2a9cd2dac2b924cd48309473693203aaa4f1e0d7
Closar <chebbi.aymen@hotmail.com>
2020-09-09 20:21:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,8 @@ def capitaliser_pays(nom):
     if pl >= 97 and pl <= 122: 
         pl = pl - 32
     for j in range(len(nom)):
-        if j != 0 and isspace(nom[j]) = false 
+        if j != 0 and isspace(nom[j]) = false:
+             
 
     return nom
 
