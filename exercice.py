bead7771acb495f3b9ac2b88bcb5a4951e83ef8c
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:56:30 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def capitaliser_pays(nom):
-    print(nom.fi)
+    print(nom.find('and'))
     pl = ord(nom[0])
     if pl >= 97 and pl <= 122:
         pl = pl - 32
