f71cf1af30c19c5e52b8a992bdc1b7e55b5052ad
Closar <chebbi.aymen@hotmail.com>
2020-09-09 20:22:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,8 @@ def capitaliser_pays(nom):
         pl = pl - 32
     for j in range(len(nom)):
         if j != 0 and isspace(nom[j]) = false:
-            l = ord(nom[j]) 
+            l = ord(nom[j])
+             
 
     return nom
 
