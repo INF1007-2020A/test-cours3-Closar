76b7edd3ad96d470e77fda25a09eb289045a3468
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:40:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,9 +4,9 @@ def majuscule(mot):
     for j in range(len(mot)):
 
         l = ord(mot[j])
-            if l > 64 and l <= 90:
-                l = l - 32
-                mot = mot[0:j] + chr(l) + mot[j+1:]
+        if l > 64 and l <= 90:
+            l = l - 32
+            mot = mot[0:j] + chr(l) + mot[j+1:]
     return mot
 
 
