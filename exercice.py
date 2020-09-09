64fb8a498769cd1a53073056a881d0519c6406c0
Closar <chebbi.aymen@hotmail.com>
2020-09-09 21:45:50 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,8 +9,8 @@ def majuscule(mot):
         else :
             print (l)
             print(chr(l))
-        elif l == 130:
-            l = 69
+        #elif l == 130:
+            #l = 69
             mot = mot[0:j] + chr(l) + mot[j+1:]
     return mot
 
